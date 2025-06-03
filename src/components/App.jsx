ae04c411a16268ac57c322bcdcff2f3f71e83973
Evgeniy <evgnec@gmail.com>
2025-06-03 20:54:30 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,50 @@
 import { Component } from 'react';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList/ContactList';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 import Filter from './Filter/Filter';
-
 
 class App extends Component {
   state = {
-    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
-      filter: '',
-  }
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-  
-  handleSubmit = (e) => {
-    e.preventDefault();  
+  handleSubmit = e => {
+    e.preventDefault();
     const form = e.currentTarget;
-    const name = form.elements.name.value;   
-    const phone = form.elements.number.value;    
+    const name = form.elements.name.value;
+    const phone = form.elements.number.value;
     const contact = {
       id: nanoid(),
-      name:name,
+      name: name,
       number: phone,
-    }; 
+    };
     this.setState(({ contacts }) => ({
       contacts: [...contacts, contact],
     }));
     form.reset();
   };
 
-  handleChange = (e) => {
-    this.setState({filter: e.currentTarget.value});
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
   };
+
   render() {
     const { contacts, filter } = this.state;
     return (
       <>
-      <h1>Phonebook</h1>
-      <ContactForm handleSubmit={this.handleSubmit}/>
+        <h1>Phonebook</h1>
+        <ContactForm handleSubmit={this.handleSubmit} />
 
-<Filter filter={filter} handleChange={this.handleChange}/>
-      <h2>Contacts</h2>
-      <ContactList contacts={contacts}/>
+        <Filter filter={filter} handleChange={this.handleChange} />
+        <h2>Contacts</h2>
+        <ContactList contacts={contacts} filter={filter} />
       </>
     );
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 // import css from './ContactList.module.css';
 
-const ContactList = ({ contacts }) => (
-  <ul>
-    {contacts.map(({ id, name, number }) => (
-      <li key={id}>
-        <p>{name}</p> <p>{number}</p>
-      </li>
-    ))}
-  </ul>
-);
+const ContactList = ({ contacts, filter }) => {
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
+  return (
+    <ul>
+      {filteredContacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <p>{name}</p>
+          <p>{number}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ContactList;
